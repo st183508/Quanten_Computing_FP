d32dd729415c64a7e9769ee89074a13cbeda86fa
Pascal <st183508@stud.uni-stuttgart.de>
2026-09-14 12:18:13 +0200

    edited

--- playground/test.py
+++ playground/test.py
@@ -1,4 +1,4 @@
 import quanten_computing_fp as qc
 import numpy as np
 
-print(np.matmul(qc.pauli_x(),qc.pauli_y()))
+print(np.matmul(qc.pauli_x(), qc.pauli_y()))
--- src/quanten_computing_fp/__init__.py
+++ src/quanten_computing_fp/__init__.py
@@ -1,13 +1,17 @@
-import numpy as np 
+import numpy as np
+
 
 def hello() -> str:
     return "Hello from quanten-computing-fp!"
 
+
 def pauli_x():
-    return np.array([[0,1],[1,0]])
+    return np.array([[0, 1], [1, 0]])
+
 
 def pauli_y():
-    return np.array([[0,-1j],[1j,0]])
+    return np.array([[0, -1j], [1j, 0]])
+
 
 def pauli_z():
-    return np.array([[1,0],[0,-1]])
+    return np.array([[1, 0], [0, -1]])
